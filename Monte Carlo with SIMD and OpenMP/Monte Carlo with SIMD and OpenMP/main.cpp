3c39ed7ef54cb0e6934a3c8e64cd0001c0c854fc
Anton94 <antonvdudov@gmail.com>
2016-12-06 11:32:21 +0200

    Started project with visual studio 2015

---
+++ Monte Carlo with SIMD and OpenMP/Monte Carlo with SIMD and OpenMP/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Github repository: TODO
+ *
+ *
+ * Finding the total volume of spheres using the Monte Carlo's method.
+ * Also using SIMD and OpenMP
+ *
+ *
+ */
+
+#include <stdlib.h>
+
+int main()
+{
+
+	return 0;
+}
